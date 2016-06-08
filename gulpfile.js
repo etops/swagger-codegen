50c25df5d300ced7de5ca9295c560137de1c2070
Christian Käslin <c.kaeslin@gmx.ch>
2016-06-08 11:03:39 +0200

    default no client dir in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var watch = require('gulp-watch');
 var batch = require('gulp-batch');
 var shell = require('gulp-shell');
 
-var buildDir = process.env.BUILD_DIR || 'client';
+var buildDir = process.env.BUILD_DIR || '';
 var javaSrc = process.env.JAVA_SRC  || 'aurora';
 var name = process.env.CODEGEN_NAME || 'AuroraCodegen';
 var targetDir = process.env.TARGET_DIR || 'myClient';
 var swaggerDefPort = process.env.SWAGGER_DEF_PORT || '3000';
 var swagger_ip = process.env.SWAGGER_DEF_IP || 'swaggerdeflink';
 /**
- * 
+ *
  */
 gulp.task('build', function () {
     gulp.src('')
@@ -26,7 +26,7 @@ gulp.task('generate', ['build'], function () {
      gulp.src('')
         .pipe(shell([
                 'java -cp /src/' +buildDir + '/' + javaSrc + '/target/' + name + '-swagger-codegen-1.0.0.jar:/opt/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar io.swagger.codegen.SwaggerCodegen generate -l ' + name + ' -i http://' + swagger_ip + ':' + swaggerDefPort + '/swagger/ -o /src/' + buildDir + '/' + targetDir
-                
+
         ]))
         .pipe(shell([
             'cd /src/' + buildDir + ' && ./beautifyAndCopy.sh'
@@ -37,5 +37,5 @@ gulp.task('buildAndGenerate', ['build', 'generate']);
 
 
 gulp.task('default', ['buildAndGenerate'], function() {
- 
+
 });
